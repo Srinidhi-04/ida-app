eb74a4e69ddfa883146af3ad157cb36acc150875
vsmart-06 <srivishnuvusirikala@gmail.com>
2025-07-06 16:48:33 +0530

    Renamed package

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.src"
+    namespace = "com.ida_app.src"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.src"
+        applicationId = "com.ida_app.src"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- src/android/app/src/main/kotlin/com/ida_app/src/MainActivity.kt
+++ src/android/app/src/main/kotlin/com/ida_app/src/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.src
+package com.ida_app.src
 
 import io.flutter.embedding.android.FlutterActivity
 
