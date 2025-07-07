f82d583fa20cdd345b9b87372bfd25777b9709af
vsmart-06 <srivishnuvusirikala@gmail.com>
2025-07-07 16:27:26 +0530

    Added some sort of check to prevent scheduler for starting multiple times

--- api/ida_app/apps.py
+++ api/ida_app/apps.py
@@ -1,11 +1,13 @@
 from django.apps import AppConfig
+import os
 
 class IdaAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ida_app'
 
     def ready(self):
-        from ida_app.tasks import scheduler, start_scheduler
+        if os.getenv("RUN_MAIN") == "true":
+            from ida_app.tasks import scheduler, start_scheduler
 
-        if not scheduler.running:
-            start_scheduler()
+            if not scheduler.running:
+                start_scheduler()
