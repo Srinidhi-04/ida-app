244d3d3a1bb31962ac193b9539b06c421861c466
vsmart-06 <srivishnuvusirikala@gmail.com>
2025-07-11 12:50:56 +0530

    Added location details for ios

--- src/ios/Runner/AppDelegate.swift
+++ src/ios/Runner/AppDelegate.swift
@@ -9,6 +9,7 @@ import UserNotifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("GOOGLE_API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     FirebaseApp.configure()
     UNUserNotificationCenter.current().delegate = self
