5a17671f6a9127bcd541167699fb0d24f6c8debe
vsmart-06 <srivishnuvusirikala@gmail.com>
2025-07-20 12:59:04 +0530

    Change token after 30 days

--- api/ida_app/views.py
+++ api/ida_app/views.py
@@ -171,6 +171,10 @@ def login(request: HttpRequest):
         return JsonResponse({"message": "Code successfully resent", "user_id": user.user_id, "email": user.email})
 
     if user:
+        if datetime.datetime.now() - user.last_login > datetime.timedelta(days = 30):
+            uid = uuid.uuid4()
+            user.token = uid.hex
+
         user.last_login = datetime.datetime.now()
         user.save()
 
