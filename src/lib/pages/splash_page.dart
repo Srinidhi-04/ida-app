9865d1eeaa6e2ae50d3e521142f0ce1108074ea3
vsmart-06 <srivishnuvusirikala@gmail.com>
2025-08-06 16:12:12 +0530

    Changed app version

--- src/lib/pages/splash_page.dart
+++ src/lib/pages/splash_page.dart
@@ -34,7 +34,7 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  final app_version = 9.0;
+  final app_version = 10.0;
   bool update = false;
 
   String baseUrl = "https://ida-app-api-afb7906d4986.herokuapp.com/ida-app";
